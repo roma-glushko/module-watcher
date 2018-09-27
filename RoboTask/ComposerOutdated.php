b95b47519f7927fe84571f97de0dd08266cff97f
Roman Glushko <r.glushko@atwix.com>
2018-09-27 19:57:42 +0300

    Added copyrights

--- RoboTask/ComposerOutdated.php
+++ RoboTask/ComposerOutdated.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * This file is part of project-update-watcher <https://github.com/roma-glushko/project-update-watcher>
+ *
+ * @author Roman Glushko <https://github.com/roma-glushko>
+ */
 
 namespace ProjectUpdateWatcher\RoboTask;
 
