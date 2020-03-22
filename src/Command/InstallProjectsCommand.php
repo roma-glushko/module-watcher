2d2b0250323ea3e2cf09a03ee60fc34ade881ff2
Roman Glushko <r.glushko@atwix.com>
2020-03-22 17:17:04 +0200

    📦 Inited notification channels architecture

--- src/Command/InstallProjectsCommand.php
+++ src/Command/InstallProjectsCommand.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace ModuleWatcher\Command;
 
+use ModuleWatcher\Config\ConfigManager;
+use ModuleWatcher\Project\NotificationManager;
 use ModuleWatcher\Project\ProjectFetcher;
+use Monolog\Logger;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -33,7 +36,16 @@ class InstallProjectsCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        (new ProjectFetcher())->fetch('git@github.com:X/Y.git', 'tmp/');
+        //(new ProjectFetcher())->fetch('git@github.com:Atwix/Cabinets-m2.git', 'tmp/cab');
+        //var_dump((new ConfigManager('module-watcher.yaml'))->getProjects());
+
+        $configManager = new ConfigManager('module-watcher.yaml');
+        $notificationLogger = (new NotificationManager($configManager))->getNotificationLogger(
+            'cabinetsdotcom',
+            'master'
+        );
+
+        $notificationLogger->log(Logger::INFO, 'TEST');
 
         return 0;
     }
